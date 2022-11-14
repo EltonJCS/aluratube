07da5e4ef4d03bac6001c46bd2e07289c8164493
EltonJCS <jcs.elton@gmail.com>
2022-11-13 21:36:02 -0300

    Minor style changes

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledTimeline = styled.div`
   flex: 1;
   width: 100%;
-  padding: 16px;
   overflow: hidden;
   h2 {
     font-size: 16px;
@@ -24,20 +23,24 @@ export const StyledTimeline = styled.div`
     overflow: hidden;
     padding: 16px;
     div {
+      background-color: ${({ theme }) => theme.backgroundLevel2 || "#222222"};
       width: calc(100vw - 16px * 4);
+      border-radius: 1rem;
       display: grid;
       grid-gap: 16px;
       grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
       grid-auto-flow: column;
       grid-auto-columns: minmax(200px, 1fr);
       overflow-x: scroll;
+      scrollbar-width: thin;
       scroll-snap-type: x mandatory;
       a {
+        background-color: ${({ theme }) => theme.backgroundLevel1 || "#222222"};
         scroll-snap-align: start;
         span {
           padding-top: 8px;
+          margin: 0 4px;
           display: block;
-          padding-right: 24px;
           color: ${({ theme }) => theme.textColorBase || "#222222"};
         }
       }
